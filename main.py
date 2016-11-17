b44db5dba51b7d1183f0b1605178b80e3c55eda5
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2016-11-17 11:32:46 +0530

    Added slack notification

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print "Hello World"
+import requests
+import json
+import os
+
+def slack_notification(message):
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "channel" : "#email"
+        "text": "In KWOC Website following error occured :\n{}".format(message)
+    })
+    r = requests.post(
+        os.environ["SLACK_WEBHOOK_URL"], headers=headers, data=data)
+
+    if r.status_code != 200:
+        print("in slack_notification : {}".format(r.status_code))
+        print(r.text)
+
+slack_notification("Hello World")
